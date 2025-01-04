429ea0e2ebc9affd560bced1cf0aecb6611d03c7
Anshuman <okanshuman@gmail.com>
2025-01-04 11:44:58 +0530

    updated code to show the details of stock over the date and also date wise the quanity and the price

--- web_app.py
+++ web_app.py
@@ -1,5 +1,5 @@
 # web_app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 from collections import defaultdict
 
@@ -13,15 +13,38 @@ db_params = {
     'host': 'localhost'
 }
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
     
+    if request.method == 'POST':
+        # Get the selected date from the form
+        selected_date = request.form['date']
+        cursor.execute("SELECT * FROM stock_holding_dhan WHERE date = %s ORDER BY trading_symbol ASC", (selected_date,))
+        records = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template('index.html', records=records, date=selected_date)
+
+    # Fetch all unique dates from the database for the home page
+    cursor.execute("SELECT DISTINCT date FROM stock_holding_dhan ORDER BY date ASC")
+    dates = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+
+    return render_template('index.html', dates=dates)
+
+@app.route('/quantity_changes')
+def quantity_changes():
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+
     # Fetch all records from the database
     cursor.execute("SELECT * FROM stock_holding_dhan ORDER BY date ASC")
     records = cursor.fetchall()
-    
+
     cursor.close()
     conn.close()
 
@@ -34,7 +57,7 @@ def index():
         data_by_symbol[trading_symbol][date].append(total_qty)
 
     # Prepare a list of stocks with quantity differences including dates
-    quantity_changes = []
+    quantity_changes_list = []
 
     for symbol, dates in data_by_symbol.items():
         sorted_dates = sorted(dates.keys())  # Sort dates for chronological order
@@ -45,13 +68,12 @@ def index():
             current_qty = dates[date][0]  # Get current quantity for that date
             
             if previous_date is not None:
-                quantity_changes.append((symbol, previous_date, previous_qty, date, current_qty))
+                quantity_changes_list.append((previous_date, date, symbol, previous_qty, current_qty))
             
             previous_date = date  # Update previous date for next iteration
             previous_qty = current_qty  # Update previous quantity for next iteration
 
-    # Render HTML using the index.html file located in the root directory.
-    return render_template('index.html', quantity_changes=quantity_changes)
+    return render_template('index.html', quantity_changes=quantity_changes_list)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5003)  # Change host to 0.0.0.0 if needed.
