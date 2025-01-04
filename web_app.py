1eba364247f1c480751c21e28dbb753cf3c04e2a
Anshuman <okanshuman@gmail.com>
2025-01-04 13:11:27 +0530

    updated the latest changes

--- web_app.py
+++ web_app.py
@@ -23,8 +23,10 @@ def index():
         cursor.execute("SELECT * FROM stock_holding_dhan WHERE date = %s ORDER BY trading_symbol ASC", (selected_date,))
         records = cursor.fetchall()
         
-        # Calculate profit/loss for each stock
+        # Calculate profit/loss for each stock and total profit/loss
         stocks_with_profit_loss = []
+        total_profit_loss = 0  # Initialize total profit/loss
+        
         for record in records:
             date = record[1]  # Date
             trading_symbol = record[2]  # Trading Symbol
@@ -36,12 +38,14 @@ def index():
             profit_loss = (last_traded_price - avg_cost_price) * total_qty
             
             stocks_with_profit_loss.append((date, trading_symbol, total_qty, avg_cost_price, last_traded_price, profit_loss))
+            total_profit_loss += profit_loss  # Accumulate total profit/loss
         
         total_count = len(stocks_with_profit_loss)  # Count of stocks
         
         cursor.close()
         conn.close()
-        return render_template('index.html', records=stocks_with_profit_loss, date=selected_date, total_count=total_count)
+        return render_template('index.html', records=stocks_with_profit_loss, date=selected_date, 
+                               total_count=total_count, total_profit_loss=total_profit_loss)
 
     # Fetch all unique dates from the database for the home page
     cursor.execute("SELECT DISTINCT date FROM stock_holding_dhan ORDER BY date ASC")
